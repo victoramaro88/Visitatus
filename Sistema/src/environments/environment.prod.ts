aa87f429d8b3a39a1016e14287993ee4fe2c7d6c
Victor Amaro <amaro.victor@hotmail.com>
2025-02-07 10:15:41 -0300

    .

--- Sistema/src/environments/environment.prod.ts
+++ Sistema/src/environments/environment.prod.ts
@@ -1,9 +1,7 @@
 export const environment = {
   production: true,
 
-  version: '1.0.0 | 09/10/2024-07:52 | Produção',
+  version: '1.0.0 | 01/02/2024-09:03 | Produção',
 
-  apiServicos: "https://www.victoramaro.com.br/BetterLife/_API/api",
-  urlValidaCarteira: "https://www.victoramaro.com.br/BetterLife/valida?data=",
-  linkExibirCarteira: "https://www.victoramaro.com.br/BetterLife/carteira?data="
+  apiServicos: 'https://visitatus.com.br/_API/api',
 };
